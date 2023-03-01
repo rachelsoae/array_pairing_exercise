d1fc566fc4ece1026af2ff1145e3fd253ca355ca
Rachel Prather <rachelprather1@gmail.com>
2023-03-01 15:25:42 -0800

    Add placesToVisit array

--- friends.js
+++ friends.js
@@ -1,4 +1,7 @@
-var arrays = ["Sarah", "Jade", "Dennis", "Brenda", "Carly"];
-console.log(arrays[2]);
-console.log(arrays[4]);
-console.log(arrays[0]);
+// var arrays = ["Sarah", "Jade", "Dennis", "Brenda", "Carly"];
+// console.log(arrays[2]);
+// console.log(arrays[4]);
+// console.log(arrays[0]);
+
+var placesToVisit = ["place 1", "place 2", "place 3", "place 4", "place 5"];
+console.log(placesToVisit.slice(1, 4))
